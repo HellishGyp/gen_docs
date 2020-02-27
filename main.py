1ca491a0e8dec93f2e1dafc1368bb717de76a3a0
hellishgyp <hellofromthehell@gmail.com>
2020-02-27 21:38:06 +0300

    add main&template

---
+++ main.py
@@ -0,0 +1,18 @@
+import datetime
+
+from docxtpl import DocxTemplate
+
+now = datetime.datetime.now()
+doc = DocxTemplate("template.docx")
+context = {'company': "CompanyName",
+           'emploer_name': "ФИО_0",
+           'type': "Акт приема-передачи оборудования",
+           'doc_name': "доверенности №123456",
+           'emploee_name': "ФИО_1",
+           'doc_initials': "Инициалы_0",
+           'emp_initials': "Инициалы_1",
+           'date': now}
+name, time, = context['emploee_name'], context['date'],
+header = f'{type}-{name}'
+doc.render(context)
+doc.save(header + '.docx')
